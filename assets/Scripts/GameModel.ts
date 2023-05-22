403cacd42a38fadc852e5004e2fc61ada90e7070
ts092000 <ts092000@gmail.com>
2023-05-22 08:59:10 +0700

    Set up base project

---
+++ assets/Scripts/GameModel.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('GameModel')
+export class GameModel extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
